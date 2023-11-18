1ce56373b15c7a36a9b03559ffe09b3f209e3bb8
GoatProgrammer <infernodia@gmail.com>
2023-11-17 21:01:01 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFshDuLrzkbTIPicG0Y4gaW1vJXiLQRWQ",
-  authDomain: "slack-clone-88c69.firebaseapp.com",
-  databaseURL: "https://slack-clone-88c69.firebaseio.com",
-  projectId: "slack-clone-88c69",
-  storageBucket: "slack-clone-88c69.appspot.com",
-  messagingSenderId: "736524300691",
-  appId: "1:736524300691:web:54b87a93cc0cc3b053b866",
-  measurementId: "G-P5G7XT792S"
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
